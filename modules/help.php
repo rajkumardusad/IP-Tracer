7c9710ceeba1892ec26bdaad63d0f1c93c8f97de
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2018-11-13 21:29:50 +0530

    IP-Tracer v1.0

--- modules/help.php
+++ modules/help.php
@@ -2,13 +2,12 @@
 function help() {
     logo();
     echo <<<EOL
-
 \033[01;37m -----------------------------------------------
 |         \033[01;36mcommand\033[01;37m        |        \033[01;36mUse\033[01;37m           |
  -----------------------------------------------
-| \033[01;32mip-tracer -m\033[01;37m           | \033[01;33mTrack your IP\033[01;37m        |
-| \033[01;32mip-tracer -t traget-ip\033[01;37m | \033[01;33mTrack IP\033[01;37m             |
-| \033[01;32mip-tracer\033[01;37m              | \033[01;33mFor more information\033[01;37m |
+| \033[01;32mtrace -m\033[01;37m               | \033[01;33mTrack your IP\033[01;37m        |
+| \033[01;32mtrace -t <traget-ip>\033[01;37m   | \033[01;33mTrack IP\033[01;37m             |
+| \033[01;32mtrace --help\033[01;37m           | \033[01;33mFor more information\033[01;37m |
  -----------------------------------------------
 
 \033[01;33m Note :- \033[01;31mip-api will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
