13323227a8e178b848041e902b168dab2471744a
Rajkumar Dusad <32831684+Rajkumrdusad@users.noreply.github.com>
2019-03-22 07:43:42 +0530

    ip-tracer v1.0

---
+++ modules/.uninstall.php
@@ -0,0 +1,53 @@
+<?php
+include("system.php");
+class un {
+  public function uni() {
+    global $system;
+    // removing old files
+    if ($system=="termux") {
+      system("rm -rf /data/data/com.termux/files/usr/share/IP-Tracer");
+      system("rm -rf /data/data/com.termux/files/usr/bin/ip-tracer");
+      system("rm -rf /data/data/com.termux/files/usr/bin/trace");
+    } elseif ($system=="ubuntu") {
+      system("sudo rm -rf /usr/bin/ip-tracer");
+      system("sudo rm -rf /usr/bin/trace");
+      system("sudo rm -rf /usr/share/IP-Tracer");
+    } else {
+      system("rm -rf /usr/bin/ip-tracer");
+      system("rm -rf /usr/bin/trace");
+      system("rm -rf /usr/share/IP-Tracer");
+    }
+  }
+  function logo() {
+    system("clear");
+    echo <<<EOL
+\033[01;33m
+
+
+\033[01;31m      _\033[01;33m ____    _
+     \033[01;31m(_)\033[01;33m  _ \  | |_ _ __ __ _  ___ ___ _ __
+     | | |_) | | __| '__/ _` |/ __/ _ \ '__|
+     | |  __/  | |_| | | (_| | (_|  __/ |
+     |_|_|      \__|_|  \__,_|\___\___|_|
+
+
+    \033[01;37m}\033[01;31m--------------------------------------\033[01;37m{
+ }\033[01;31m------------- \033[01;32mTrack IPLocation\033[01;31m -------------\033[01;37m{
+    }\033[01;31m--------------------------------------\033[01;37m{
+
+\033[00m
+EOL;
+
+    if (file_exists("/usr/bin/ip-tracer")) {
+      echo "\n\n\033[01;31m  Sorry IP-Tracer is not removed !!!\033[00m";
+    } else if(file_exists("/data/data/com.termux/files/usr/bin/ip-tracer")) {
+      echo "\n\n\033[01;31m  Sorry IP-Tracer is not removed !!!\033[00m";
+    } else {
+      echo "\n\n\033[01;32m  IP-Tracer is uninstalled !!!\033[00m";
+    }
+  }
+}
+$a=new un;
+$a->uni();
+$a->logo();
+?>
